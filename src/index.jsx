00ef9591a73ac0b75d4feb1419110e3dfe60aee0
Gabriel Garcia <gabrielgarcia.anjos250@gmail.com>
2022-05-17 17:47:40 -0300

    Configurando

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './styles/global-styles';
+import { ThemeProvider } from 'styled-components';
 import Home from './templates/App';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
 );
