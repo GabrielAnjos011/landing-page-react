d528b4dc53b78d361ff685de8d6ce88837920a3f
Gabriel Garcia <gabrielgarcia.anjos250@gmail.com>
2022-05-20 17:30:26 -0300

    Criação de componente

---
+++ src/components/TextComponents/index.jsx
@@ -0,0 +1,9 @@
+import * as Styled from './styles';
+
+export function TextComponents() {
+  return (
+    <Styled.Container>
+      <h1>TextComponents</h1>
+    </Styled.Container>
+  );
+}
--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -2,14 +2,22 @@ import { css } from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+
     * {
         margin: 0;
         padding: 0;
     }
 
+    html {
+        font-size: 62.5%;
+    }
+
     body {
-        ${({ theme }) => css`
-          background: ${theme.colors.mainBg};
-        `}
+        font-size: 1.6rem;
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    h1 {
+        font-family: 'Montserrat', sans-serif;
     }
 `;
